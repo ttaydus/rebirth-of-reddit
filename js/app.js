553f027fc7c2d9737b775eae4bc42242b6379f32
Tyler <tylertaydus4444@gmail.com>
2019-02-05 14:14:20 -1000

    trying to add via forEach

--- js/app.js
+++ js/app.js
@@ -12,9 +12,21 @@ const request = (url, callback) => {
   request('http://www.reddit.com/r/TheOcho.json', function(data) {
     // console.log('data:', data);
     const individualPost = data.data.children;
-    // console.log('individualPost:', individualPost);
-    request(individualPost[4].data.thumbnail, function(data){
-        console.log(data)
+    console.log('individualPost:', individualPost);
+    individualPost.forEach((post) => {
+        // const author = post.data.author;
+        // const title = post.data.title;
+        // const mainText = post.data.selftext;
+
+        // const getPostsBlock = document.getElementsByClassName('postsBlock');
+        // const newPostFrame = document.createElement('div');
+        // newPostFrame.className = 'postFrame';
+        // getPostsBlock.appendChild(newPostFrame);
+        
+        // const newPostTitle = document.createElement('div');
+        // newPostTitle.className = 'postTitle';
+        // newPostTitle.innerHTML = title;
+        // newPostFrame.appendChild(newPostTitle); 
     });
   });
 
@@ -93,41 +105,6 @@ const request = (url, callback) => {
   // url = link or permalink
   // permalink = post page and comments 
   // thumbnail = starting img of video
-
-
-
-
-// var person4Req = new XMLHttpRequest();
-// person4Req.addEventListener('load', function(data){
-
-//     //console.log(data.target.responseText);
-//     const person4Obj = JSON.parse(data.target.responseText);
-//     //console.log('person4Obj:', person4Obj);
-    
-//     const getPerson4Name = document.getElementById('person4Name');
-//     getPerson4Name.innerHTML = person4Obj.name;
-
-//     //// NEW REQUEST FOR HOMEWORLD LINK
-
-//     var planetReq = new XMLHttpRequest();
-//     planetReq.addEve"https://i.redd.it/3s7ega4808e21.jpg"ntListener('load', function(data){
-//         const homeWorld4 = JSON.parse(data.target.responseText);
-    
-//         //console.log(homeWorld4)
-
-//         const getPerson4HomeWorld = document.getElementById('person4HomeWorld');
-//         getPerson4HomeWorld.innerHTML = homeWorld4.name;
-//     });
-
-//     planetReq.open('GET', "https://swapi.co/api/planets/1/");
-//     planetReq.send();
-    
-// });
-
-// person4Req.open('GET', 'https://swapi.co/api/people/4/');
-// person4Req.send();
-
-
 
 
 
